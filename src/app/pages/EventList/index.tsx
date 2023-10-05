b673e0b7c2538db8df06e6580cd59ae345567e86
Albert <albert.yu.zh@gmail.com>
2023-10-04 18:18:56 -0700

    fix lint error

--- src/app/pages/EventList/index.tsx
+++ src/app/pages/EventList/index.tsx
@@ -48,7 +48,7 @@ const EventList: React.FC = () => {
             <div className={styles.footer}>
                 <Tabbar >
                     <Tabbar.Item title="首页" icon={<Home width={18} height={18} />} value={9} />
-                    <Tabbar.Item title="分类" icon={<Category width={18} height={18} dot />} />
+                    <Tabbar.Item title="分类" icon={<Category width={18} height={18} />} />
                     <Tabbar.Item title="发现" icon={<Find width={18} height={18} />} />
                     <Tabbar.Item title="购物车" icon={<Cart width={18} height={18} />} />
                     <Tabbar.Item title="我的" icon={<My width={18} height={18} />} />
